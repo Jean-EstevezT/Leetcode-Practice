175fba6e1deb5c5994e8893e294e4b42047501ba
Jean Estevez <ctarriba9@gmail.com>
2021-02-11 21:56:47 -0400

    #34 75

--- Array/javascript/sort-colors.js
+++ Array/javascript/sort-colors.js
@@ -1,8 +1,7 @@
 //Title     : 75. Sort Colors
 //Category  : Array
 //URL       : https://leetcode.com/problems/sort-colors
-//submission: https://leetcode.com/submissions/detail/452562834/
-
+//submission: https://leetcode.com/submissions/detail/454640535/
 
 //--------------------------------------------------
 
@@ -11,34 +10,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 let sortColors = function (nums) {
-  //let len = nums.length;
-  //for (let i = 1; i < len; i++) {
-  //for (let j = 0; j < len - i; j++) {
-  //if (nums[j] > nums[j + 1]) {
-  //let temp = nums[j]
-  //nums[j] = nums[j + 1];
-  //nums[j + 1] = temp;
-  //}
-  //}
-  //}
-  //return nums;
-
-  let flag = true
-  while (flag) {
-    flag = false;
-    for (let i = 0; i < nums.length - 1; i++) {
-      if (nums[i] > nums[i + 1]) {
-        let temp = nums[i]
-        nums[i] = nums[i + 1];
-        nums[i + 1] = temp;
-        flag = false;
+  let len = nums.length;
+  for (let i = 1; i < len; i++) {
+    for (let j = 0; j < len - i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        let temp = nums[j]
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
       }
     }
   }
   return nums;
 };
 
-
-console.log(sortColors([2, 0, 2, 1, 1, 0]))
-console.log(sortColors([2, 0, 1]))
 
